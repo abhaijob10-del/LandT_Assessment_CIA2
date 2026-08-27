c0239b47bec928a0df834741ce97bdeed40b8613
Abhai Job KJ <abhaijob10@gmail.com>
2026-08-27 23:45:08 +0530

    Create Task6.js

---
+++ Task6.js
@@ -0,0 +1,8 @@
+"scripts": {
+  "test": "echo "Error: no test specified\" && exit 1",
+  "dev": "nodemon countdown.js",
+  "start": "node countdown.js"
+}, 
+"devDependencies": {
+  "nodemon": "^3.1.14"
+}
